4937a5952f69292c3e58d9e6152688c02d3d12a5
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-02-21 22:09:23 -0800

    Add player choice functionality
    Added functions in game.js that let a player choose a choice among rock, paper, scissors.
    Also prompts the player for the correct choice if they decide to enter something other than
    the three choices. The input is also case insensitive.

--- game.js
+++ game.js
@@ -1,3 +1,32 @@
+
+let playerChoice = prompt("Please choose rock, paper, or scissors: ").toLowerCase();
+console.log(playerChoice);
+if (playerChoice === ("rock" || "paper" || "scissors")) {
+    console.log(playerChoice);
+    console.log("Congrats, you followed directions.")
+    } else {
+    youFuckedUp();
+    console.log("Congrats, you finally did it.");
+    }
+
+function youFuckedUp() {
+    let fuckedUpState = 1;
+    while (fuckedUpState === 1) {
+        let playerChoice = prompt("Please choose rock, paper, or scissors...or else: ").toLowerCase();
+        switch (playerChoice) {
+            case "rock":
+                fuckedUpState = 0;
+                break;
+            case "paper":
+                fuckedUpState = 0;
+                break;
+            case "scissors":
+                fuckedUpState = 0;
+                break;
+        }
+    }
+    return playerChoice;
+}
 function computerChoice() {
     let randomNumber = Math.floor(Math.random() * 100);
     compAnswer = computerRNG(randomNumber);
